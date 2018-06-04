f4f9b16405c00a982243ffab398cd9fe36170cac
Mike Mitterer <office@mikemitterer.at>
2018-06-04 19:59:54 +0200

    Version updated

--- web/git.version.dart
+++ web/git.version.dart
@@ -1,6 +1,6 @@
 /// Do not changed this file manually.
-/// Generated at: 2018-06-04 19:54:23.818982
+/// Generated at: 2018-06-04 19:59:05.572075
 ///     AssetId: mini_web_sample|web/$web$
-///     Version: 1.0
+///     Version: 1.0.1
 
-final String _version = "1.0";
+final String _version = "1.0.1";
