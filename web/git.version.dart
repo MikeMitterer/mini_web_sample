84f2a0339f626455d545a7abd83af1c743aecb4a
Mike Mitterer <office@mikemitterer.at>
2018-06-28 15:54:51 +0200

    Dart 2.x ready

--- web/git.version.dart
+++ web/git.version.dart
@@ -1,6 +1,6 @@
 /// Do not changed this file manually.
-/// Generated at: 2018-06-06 11:11:10.277814
+/// Generated at: 2018-06-28 15:52:15.411353
 ///     AssetId: mini_web_sample|web/$web$
-///     Version: 1.0.4
+///     Version: 1.0.5
 
-final String gitVersion = "1.0.4";
+final String gitVersion = "1.0.5";
